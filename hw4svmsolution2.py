71d209590e460e0a4fca19a3c83c74a45760f59a
vikas753 <vvikas1996@gmail.com>
2020-04-12 23:54:22 -0700

    Update hw4svmsolution2.py

--- hw4svmsolution2.py
+++ hw4svmsolution2.py
@@ -28,7 +28,7 @@ def show_plot_svm_contour(data,svm,filename,labels):
         
     x_min, x_max = dataX.min() - 1, dataX.max() + 1
     y_min, y_max = dataY.min() - 1, dataY.max() + 1
-    h = .02
+    h = 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                          np.arange(y_min, y_max, h))
     plt.scatter(dataX,dataY,c=labels,cmap=plt.cm.coolwarm)
@@ -66,7 +66,6 @@ def svmtraining(regularization,gamma,data,labels):
     model = SVC(C=regularization,gamma=gamma,kernel='rbf')
     model.fit(data, labels)
     #plot_scatter_sh(data,labels,"")
-    #plot_scatter_sh(data,predicted_labels,"")
     return model    
     
 ## Performs a validation of a SVM against a data set and returns score
@@ -74,8 +73,9 @@ def svmvalidate(svm,data,labels,filename):
     #plot_scatter_sh(data,labels,filename)
     predicted_labels=svm.predict(data)
     loss=accuracy_score(labels,predicted_labels)
-    print("loss=",loss)
-    #show_plot_svm_contour(data,svm,filename,labels)
+    if(filename == ""):
+        plot_scatter_sh(data,labels,"")
+        show_plot_svm_contour(data,svm,"",labels)
     return loss
     
 ## Performs a full training on the data using SVM .     
@@ -86,7 +86,8 @@ def svmFullTraining(data,labels):
     for C in [1,10,100,200,500,700,1000]:
         for Gamma in [0.00001,0.0001,0.001,0.01,0.1,1,10]:
             model=svmtraining(C,Gamma,data,labels)
-            loss=svmvalidate(model,data,labels,"")
+            loss=svmvalidate(model,data,labels,"abcd")
+            #print(" C = " , C, " Gamma = " , Gamma , "loss =" , loss )
             if(loss < min_loss):
                 min_loss = loss
                 best_model = model
@@ -120,20 +121,33 @@ def KfoldCrossValidation(data,labels,kfold):
         trainedSVM = svmFullTraining(data_train,labels_train)
         loss_dataset = svmvalidate(trainedSVM,data_test,labels_test,"modelTestExample.png")
         lossKfoldArray.append(loss_dataset)
+        print(" Validation Index = " , validationIndex , " loss_dataset = " , loss_dataset)
         
         if(loss_dataset < min_loss):
             min_loss = loss_dataset
             svmmodel = trainedSVM
     
-    plt.plot(validationIndexArray,lossKfoldArray, linewidth=5.0)
-    plt.ylabel(' Loss-K-fold Array ')
-    plt.xlabel(' Validation Index ')
-    plt.savefig('ValidationIndexGraph.png')
+    #plt.plot(validationIndexArray,lossKfoldArray, linewidth=5.0)
+    #plt.ylabel(' Loss-K-fold Array ')
+    #plt.xlabel(' Validation Index ')
+    #plt.savefig('ValidationIndexGraph.png')
      
     return svmmodel    
  
 lossArray = []
+
+with open('dataXhw42100test.csv', newline='') as f:
+    reader = csv.reader(f)
+    filedataX_toy = [tuple(row) for row in reader]
+
+with open('dataYhw42100test.csv', newline='') as f:
+    reader = csv.reader(f)
+    filedataY_toy = [tuple(row) for row in reader]
     
+with open('labels100.csv', newline='') as f:
+    reader = csv.reader(f)
+    labels_toy = [tuple(row) for row in reader]
+
 with open('dataXhw421000train.csv', newline='') as f:
     reader = csv.reader(f)
     filedataX_train = [tuple(row) for row in reader]
@@ -146,17 +160,25 @@ with open('labelshw421k.csv', newline='') as f:
     reader = csv.reader(f)
     labels_train = [tuple(row) for row in reader]
 
-with open('dataXhw4110000test.csv', newline='') as f:
+with open('dataXhw4210000test.csv', newline='') as f:
     reader = csv.reader(f)
     filedataX_test = [tuple(row) for row in reader]
 
-with open('dataYhw4110000test.csv', newline='') as f:
+with open('dataYhw4210000test.csv', newline='') as f:
     reader = csv.reader(f)
     filedataY_test = [tuple(row) for row in reader]
 
 with open('labelshw4210k.csv', newline='') as f:
     reader = csv.reader(f)
     labels_test = [tuple(row) for row in reader]
+
+filedataX_toy = [float(i) for i in filedataX_toy[0]]    
+filedataY_toy = [float(j) for j in filedataY_toy[0]]
+labels_toy = [int(k)   for k in labels_toy[0]]
+
+filedataX_toy = np.array(filedataX_toy)
+filedataY_toy = np.array(filedataY_toy)
+labels_toy = np.array(labels_toy)
 
 filedataX_train = [float(i) for i in filedataX_train[0]]    
 filedataY_train = [float(j) for j in filedataY_train[0]]
@@ -174,6 +196,10 @@ filedataX_test = np.array(filedataX_test)
 filedataY_test = np.array(filedataY_test)
 labels_test = np.array(labels_test)
 
+Data_toy = []
+
+for i in range(filedataX_toy.size):
+    Data_toy.append([filedataX_toy[i],filedataY_toy[i]])
     
 Data = []
 
@@ -184,6 +210,8 @@ Data_test = []
 
 for i in range(filedataX_test.size):
     Data_test.append([filedataX_test[i],filedataY_test[i]])
+        
+
         
 svmmodel = KfoldCrossValidation(Data,labels_train,10)   
 loss = svmvalidate(svmmodel,Data_test,labels_test,"")
